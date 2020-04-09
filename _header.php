3f8f654790116749e86f8ad2a9d9d58f48b60ce0
HeatherHyer <heather.hyer@familysearch.org>
2020-04-09 12:52:01 -0600

    php

---
+++ _header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Unit 01</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+  <link href="css/styles.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+
+  <div class="headerWrapper">
+    <header class="clearfix">
+      <figure>
+        <img class="logo" src="./images/logo1.png" alt="logo" srcset="./images/logo1.png 1x, ./images/logo2.png 2x, ./images/logo3.png 3x">
+      </figure>
+      <h1>Cabot Cruises</h1>
+      <a id="headerTel" href="tel:123-456-7890">123-456-7890</a>
+    </header>
+  </div>
+
+  <div class="navWrapper">
+    <nav class="clearfix">
+      <button id="hamburgerBtn"><span><i>&equiv;</i>OPEN</span> <span><i>&otimes;</i>CLOSE</span></button>
+      <ul id="primaryNav">
+          <li><a href="#">Home</a></li>
+          <li class="current-menu-item"><a href="#">Cruises</a></li>
+          <li class="parent"><a href="#">Agents</a>
+              <ul>
+                  <li><a href="#">Here</a></li>
+                  <li><a href="#">There</a></li>
+                  <li><a href="#">Everywhere</a></li>
+              </ul>
+          </li>
+          <li><a href="#">Book</a></li>
+      </ul>
+      <div class="searchBar"><input type="search" id="searchInput" value="Search..."><i class="fas fa-search"></i></div>
+    </nav>
+  </div>
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php include '_header.php'; ?>
+
+  <div class="mainWrapper">
+    <main>
+      <div class="slider">
+        <div>
+          <picture>
+            <source src="./images/slider1_lg.jpg" media="(min-width: 28.12rem)">
+            <source src="./images/slider1_sm.jpg" media="(max-width: 28.12rem)">
+            <img src="./images/slider1_lg.jpg" alt="slider">
+          </picture>
+        </div>
+        <div>
+          <picture>
+            <source src="./images/slider2_lg.jpg" media="(min-width: 28.12rem)">
+            <source src="./images/slider2_sm.jpg" media="(max-width: 28.12rem)">
+            <img src="./images/slider2_lg.jpg" alt="slider">
+          </picture>
+        </div>
+        <div>
+          <picture>
+            <source src="./images/slider3_lg.jpg" media="(min-width: 28.12rem)">
+            <source src="./images/slider3_sm.jpg" media="(max-width: 28.12rem)">
+            <img src="./images/slider3_lg.jpg" alt="slider">
+          </picture>
+        </div>
+        <div>
+          <picture>
+            <source src="./images/slider4_lg.jpg" media="(min-width: 28.12rem)">
+            <source src="./images/slider4_sm.jpg" media="(max-width: 28.12rem)">
+            <img src="./images/slider4_lg.jpg" alt="slider">
+          </picture>
+        </div>
+      </div>
+
+      <h3>Recent Blog Posts</h3>
+      <div class="blog">
+        <a class="blogPost post1" href="#">
+          <h4 class="top">Relax</h4>
+          <h4 class="bottom">Dec 12, 2019</h4>
+        </a>
+        <a class="blogPost post2" href="#">
+          <h4 class="top">Explore</h4>
+          <h4 class="bottom">Jan 12, 2020</h4>
+        </a>
+        <a class="blogPost post3" href="#">
+          <h4 class="top">Remember</h4>
+          <h4 class="bottom">Feb 12, 2020</h4>
+        </a>
+        <a class="blogPost post4" href="#">
+          <h4 class="top">Enjoy</h4>
+          <h4 class="bottom">Mar 12, 2020</h4>
+        </a>
+      </div>
+
+
+    </main>
+  </div>
+
+<?php include '_footer.php'; ?>
