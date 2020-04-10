a9335e78c4c9f3499d62301bb1ed66db48b7e827
HeatherHyer <heather.hyer@familysearch.org>
2020-04-10 11:41:33 -0600

    form

--- book.php
+++ book.php
@@ -2,8 +2,22 @@
 
 <div class="mainWrapper">
   <main>
-    <h2>Book</h2>
-
+    <h2>Book a Cruise</h2>
+    <form>
+      <label for="name">Full Name</label>
+      <input type="text" name="name" value="name" required>
+      <label for="address">Address</label>
+      <input type="text" name="address" value="address">
+      <label for="city">City</label>
+      <input type="text" name="city" value="city">
+      <label for="zip">Zipcode</label>
+      <input type="text" name="zip" value="zip">
+      <label for="phone">Phone Number</label>
+      <input type="text" name="ohone" value="phone">
+      <label for="email">Email Address</label>
+      <input type="email" name="email" value="email" required>
+      <button type="submit" name="submit">Book My Cruise</button>
+    </form>
 
   </main>
 </div>
