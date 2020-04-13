2a4bb39a8334b808d3f4139a3373f8da70b1401b
HeatherHyer <heather.hyer@familysearch.org>
2020-04-12 19:31:44 -0600

    agents

--- agent1.php
+++ agent1.php
@@ -3,8 +3,24 @@
 <div class="mainWrapper">
   <main>
     <h2>Max Donoghue</h2>
-
-
+    <p><b>Phone:</b></p>
+    <p>1-800-555-1234</p>
+    <p><b>Working:</b></p>
+    <p>Monday, Wednesday, Friday</p>
+    <p><b>Destination Specialization:</b></p>
+    <p>Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas, Caribbean cruises.</p>
+    <p><b>Office Hours:</b></p>
+    <p>8:00am-6:00pm</p>
+    <p><b>Biography:</b></p>
+    <p>I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself to Hawaii and I never came back. Right out of high school I started into travel school and have had the opportunity to work in many aspects of the industry. In every aspect I have worked I have had the opportunity to help people travel, to help fulfill people's dreams, what could be be6er to do every day!</p>
+    <p><b>Customer Comments:</b></p>
+    <p>"Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back such wonderful memories."</p>
+    <p>-John Doe</p>
+    <p>"Thank you for your cheery attitude and hard work during the planning of our tour!"</p>
+    <p>-The Smiths</p>
+    <p>"Thanks for your patience and efficiency. Let's do it again sometime very soon!"</p>
+    <p>-Jane and John Westagar</p>
+    <img class="agentPhoto" src="./images/agent2.jpg">
   </main>
 </div>
 
