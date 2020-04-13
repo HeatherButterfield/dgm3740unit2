569e2a3a9f560b2f73aaee0ff8eaa395dfabfee6
HeatherHyer <heather.hyer@familysearch.org>
2020-04-12 20:03:06 -0600

    br

--- cruise.php
+++ cruise.php
@@ -4,43 +4,42 @@
   <main>
     <h2>Cruises</h2>
 
-    <div class="cruises">
-      <div class="cruise">
-        <img src="./images/slider1_sm.jpg"></img>
-        <div class="cruiseDesc">
-          <h3>Caribbean Cruises starting at $400</h3>
-          <p>We don’t have proof, but evidence suggests that the Caribbean was made for cruising. This evidence is all around you — you’ll find in the Caribbean air, the sand and the water. And with more than 5,000 islands and cays spread across this amazing region, there’s a lot of paradise to see. So how do you choose where to visit on a Caribbean cruise? We recommend you just go and see for yourself!</p>
-          <button type="button" name="readMore">Read More</button>
-        </div>
-      <div>
-
-      <div class="cruise">
-        <img src="./images/slider2_sm.jpg"></img>
-        <div class="cruiseDesc">
-          <h3>Bahamas Cruises starting at $600</h3>
-          <p>You know the name, the laid-back attitude and where to find them, but you’ll just have to visit The Bahamas to truly appreciate this classic cruise destination. On this 700-strong string of sun-splashed islands dotting the blue Atlantic, the living’s easy. (And it’s not bad on a Bahamas cruise either!) The central port of Nassau is the bustling capital of the country — “bustling” is a relative term, of course — while Freeport is all chill, all the time.</p>
-          <button type="button" name="readMore">Read More</button>
-        </div>
-      <div>
-
-      <div class="cruise">
-        <img src="./images/slider3_sm.jpg"></img>
-        <div class="cruiseDesc">
-          <h3>Hawaii Cruises starting at $800</h3>
-          <p>Take a Hawaii cruise and explore exotic locations spread across four islands — the Hawaiian island chain is home to names you know, like Maui, Kona and Honolulu. These spots feature postcard-worthy waterfalls, volcanoes and some of the world’s most stunning, sunning beaches.</p>
-          <button type="button" name="readMore">Read More</button>
-        </div>
-      <div>
-
-      <div class="cruise">
-        <img src="./images/slider4_sm.jpg"></img>
-        <div class="cruiseDesc">
-          <h3>Mexico Cruises starting at $200</h3>
-          <p>So close, yet so deliciously far. Mexico might be right down there, but it exists in another world entirely. Experience a whole enchilada’s worth of rich culture and fun on a cruise to Mexico. Each port — including favorites Cozumel, Cabo San Lucas and Puerto Vallarta — contributes its own distinct local flavor to the mix.</p>
-          <button type="button" name="readMore">Read More</button>
-        </div>
+    <div class="cruise">
+      <img src="./images/slider1_sm.jpg"></img>
+      <div class="cruiseDesc">
+        <h3>Caribbean Cruises starting at $400</h3>
+        <p>We don’t have proof, but evidence suggests that the Caribbean was made for cruising. This evidence is all around you — you’ll find in the Caribbean air, the sand and the water. And with more than 5,000 islands and cays spread across this amazing region, there’s a lot of paradise to see. So how do you choose where to visit on a Caribbean cruise? We recommend you just go and see for yourself!</p>
+        <button type="button" name="readMore">Read More</button>
+      </div>
+    <div>
+    <br>
+    <div class="cruise">
+      <img src="./images/slider2_sm.jpg"></img>
+      <div class="cruiseDesc">
+        <h3>Bahamas Cruises starting at $600</h3>
+        <p>You know the name, the laid-back attitude and where to find them, but you’ll just have to visit The Bahamas to truly appreciate this classic cruise destination. On this 700-strong string of sun-splashed islands dotting the blue Atlantic, the living’s easy. (And it’s not bad on a Bahamas cruise either!) The central port of Nassau is the bustling capital of the country — “bustling” is a relative term, of course — while Freeport is all chill, all the time.</p>
+        <button type="button" name="readMore">Read More</button>
+      </div>
+    <div>
+    <br>
+    <div class="cruise">
+      <img src="./images/slider3_sm.jpg"></img>
+      <div class="cruiseDesc">
+        <h3>Hawaii Cruises starting at $800</h3>
+        <p>Take a Hawaii cruise and explore exotic locations spread across four islands — the Hawaiian island chain is home to names you know, like Maui, Kona and Honolulu. These spots feature postcard-worthy waterfalls, volcanoes and some of the world’s most stunning, sunning beaches.</p>
+        <button type="button" name="readMore">Read More</button>
+      </div>
+    <div>
+    <br>
+    <div class="cruise">
+      <img src="./images/slider4_sm.jpg"></img>
+      <div class="cruiseDesc">
+        <h3>Mexico Cruises starting at $200</h3>
+        <p>So close, yet so deliciously far. Mexico might be right down there, but it exists in another world entirely. Experience a whole enchilada’s worth of rich culture and fun on a cruise to Mexico. Each port — including favorites Cozumel, Cabo San Lucas and Puerto Vallarta — contributes its own distinct local flavor to the mix.</p>
+        <button type="button" name="readMore">Read More</button>
       </div>
     </div>
+
   </main>
 </div>
 
